d1f409b44036a0912d243750abbefbac97341f90
kashimAstro <d.longobardi@ziggurats.net>
2015-04-24 15:55:17 +0200

    update

--- src/ofxTranslated.h
+++ src/ofxTranslated.h
@@ -1,32 +1,56 @@
 #include "ofMain.h"
 #include "ofxJSON.h"
+#include "ofThread.h"
 
 //http://mymemory.translated.net/api/get?q=Hello World!&langpair=en|it
 //http://mymemory.translated.net/doc/spec.php
 
-class ofxTranslated{
+class ofxTranslated : public ofThread {
 	public:
 		string langpair;
 		string context;
-        ofxJSONElement result;
+		string dump;
+	        ofxJSONElement result;
 
 		ofxTranslated(){ }
 		~ofxTranslated(){ }
 
-		string getResponse(string translate, string da, string a){
-                string url;
-                string response;
-                url="http://mymemory.translated.net/api/get?q="+translate+"!&langpair="+da+"|"+a;
-                ofHttpResponse resp = ofLoadURL(url);
-                cout << resp.data << endl;
-                bool parsingSuccessful = result.parse(resp.data);
-                if (parsingSuccessful) {
-                    response=result["OK"]["matches"]["translation"].asString();
+                ofEvent<string> requestComplete;
+
+		void request( string command, string _da, string _a ){
+                        cmd = command;
+			da  = _da;
+			a   = _a;
+                        startThread( );
                 }
-                else{
-                    response="Failed JSON!";
-                }
-                return response;
+
+		void stop(){
+                        stopThread( );
 		}
+
+		string getResponse(string translate, string _da, string _a){
+	                string url;
+	                string response;
+	                url="http://mymemory.translated.net/api/get?q="+translate+"&langpair="+_da+"|"+_a;
+	                ofHttpResponse resp = ofLoadURL(url);
+	                bool parsingSuccessful = result.parse(resp.data);
+	                if (parsingSuccessful) {
+			    string p = result["matches"][0]["translation"].asString();
+
+	                    response=p; 
+	                }
+	                else{
+	                    response="Failed JSON!";
+	                }
+	                return response;
+			stopThread();
+		}
+
+		void threadedFunction(){
+                        string result = getResponse(cmd.c_str(),da.c_str(),a.c_str());
+                        ofNotifyEvent( requestComplete, result, this );
+                }
+
+                string cmd,da,a;
 
 };
